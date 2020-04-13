48b096361ff374ea1548d1bcfb19d7711af0eb66
Pablo Casas <casdelprog@gmail.com>
2020-04-13 17:54:40 +0200

    Primary Bean demo

--- src/main/java/guru/spf/sfgdi/controllers/MyController.java
+++ src/main/java/guru/spf/sfgdi/controllers/MyController.java
@@ -1,11 +1,18 @@
 package guru.spf.sfgdi.controllers;
 
+import guru.spf.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String sayHello(){
-        System.out.println("Hello World");
-        return "Hi Folks!";
+        return greetingService.sayGreeting();
     }
 }
